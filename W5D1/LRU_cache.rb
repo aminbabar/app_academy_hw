71f35371a5381806f5de5428790356c859f10368
aminbabar <amin.babar1@gmail.com>
2021-07-11 23:56:46 -0400

    completed a simplistic version of LRU cache

--- W5D1/LRU_cache.rb
+++ W5D1/LRU_cache.rb
@@ -1,20 +1,36 @@
   class LRUCache
-    def initialize
+    attr_reader :size
+    
+    def initialize(max_size)
+        @underlying_array = []
+        @max_size = max_size
     end
 
     def count
       # returns number of elements currently in cache
+      @underlying_array.length
     end
 
-    def add(el)
+    def add(ele)
       # adds element to cache according to LRU principle
+      @underlying_array.delete(ele) if @underlying_array.inculde?(ele)
+
+      if self.count <= self.max_size
+        @underlying_array << ele
+      else
+        @underlying_array << ele
+        @underlying_array.shift
+      end
+
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      @underlying_array[-1]
     end
 
     private
     # helper methods go here!
+
 
   end
