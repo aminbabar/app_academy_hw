3d57608239d0050551c6a59ff57651b3e3becf42
aminbabar <amin.babar1@gmail.com>
2021-07-26 01:21:11 -0400

    user auth added

--- W7D1/rails_auth/app/models/user.rb
+++ W7D1/rails_auth/app/models/user.rb
@@ -4,6 +4,32 @@ class User < ApplicationRecord
     validates :password, length: {minimum: 10,  allow_nil: true}
     before_validation :ensure_session_token
 
-    
+    def find_by_credentials(username, password)
+        user = find_by(username: username)
+        if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+            return user
+        else
+            return false
+        end
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def password=(arg)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
 
 end
