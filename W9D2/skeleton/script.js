5a75d2706065d0c3d822c05ae7ed71f21e603eb7
Amin <amin.babar1@gmail.com>
2021-08-10 00:31:57 -0400

    last changes for the day

--- W9D2/skeleton/script.js
+++ W9D2/skeleton/script.js
@@ -14,6 +14,41 @@ document.addEventListener("DOMContentLoaded", () => {
     li.addEventListener("click", toggleLi);
   });
 
+  const button = document.getElementsByClassName("favorite-submit");
+  // debugger;
+  button[0].addEventListener("click", event => {
+    event.preventDefault();
+    const input = document.querySelector(".favorite-input");
+
+    const ul = document.querySelector("#sf-places");
+
+    const newItem = document.createElement("li");
+    newItem.textContent = input.value;
+    ul.append(newItem);
+    input.value = "";
+    // const favorit 
+
+  });
+
+
+  // const handleFavoriteSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   const favoriteInput = document.querySelector(".favorite-input");
+  //   const favorite = favoriteInput.value;
+  //   favoriteInput.value = "";
+
+  //   const newListLi = document.createElement("li");
+  //   newListLi.textContent = favorite;
+
+  //   const favoritesList = document.getElementById("sf-places");
+  //   favoritesList.appendChild(newListLi);
+  // };
+
+  // const listSubmitButton = document.querySelector(".favorite-submit");
+  // listSubmitButton.addEventListener("click", handleFavoriteSubmit);
+
+
 
 
   // adding SF places as list items
