6547a26f59ced5a01d24505f432a63a468ceaa18
Amin <amin.babar1@gmail.com>
2021-08-11 00:57:51 -0400

    completed ajax hw

--- W9D3/ajax/js/index.js
+++ W9D3/ajax/js/index.js
@@ -1,5 +1,16 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b",
+    type: "GET",
+    success: function(data){
+        console.log("Data: ")
+        console.log(data);
+    },
+    error: function(error){console.log(error)}
+});
+
 
 // Add another console log here, outside your AJAX request
+console.log("After ajax request sent!")
