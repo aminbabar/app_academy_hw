ad95c8893baf79ddb39a84f99f00aa84ac73e22c
Amin Babar <amin.babar1@gmail.com>
2021-08-19 22:46:13 -0400

    hw set up

---
+++ W10D5/skeleton-2/entry.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Congrats from './congrats';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<Congrats/>, root);
+});
+
+
+
